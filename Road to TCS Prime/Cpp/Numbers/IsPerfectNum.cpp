bf95f835862750003ee7867ffb56139469e3b09b
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-09 02:26:22 +0530

    description

--- Road to TCS Prime/Cpp/Numbers/IsPerfectNum.cpp
+++ Road to TCS Prime/Cpp/Numbers/IsPerfectNum.cpp
@@ -1,4 +1,15 @@
-
+/*A perfect number is defined as a number that is the sum of its proper divisors ( all its positive divisors excluding itself).
+Example 1:
+    Input: n=6
+    Output: 6 is a perfect number
+Example 2:
+    Input: n=15
+    Output: 15 is not a perfect number
+Example 3:
+    nput: n=28
+    Output: 28 is a perfect number
+Reason:
+    For 6 and 28 , the sum of their proper divisors (1+2+3) and (1+4+7+14) is equal to  the respective numbers and for 15 it is not. */
 
 #include<bits/stdc++.h>
 using namespace std;
