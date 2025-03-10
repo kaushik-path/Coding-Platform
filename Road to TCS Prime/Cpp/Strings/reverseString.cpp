8eb71add016fdc97bad9f1628c737e8d40161145
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-11 01:18:06 +0530

    Reverse a String

--- Road to TCS Prime/Cpp/Strings/reverseString.cpp
+++ Road to TCS Prime/Cpp/Strings/reverseString.cpp
@@ -1,23 +1,24 @@
+/*
+len=    1 2 3 4 5 6
+        s t r i n g
+    i=  0 1 2 3 4 5
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
-void printstr(string& s){
-    for(int i=0; i<s.length(); i++){
-        cout<<s[i];
-    }
-}
-void isReverse(string& s){
-    int left=s[1], right= s.length();
-    while(left<=right){
+string isReverse(string& s){
+    int left=0, right= s.length()-1;
+    while(left<right){
         swap(s[left],s[right]);
         left++;
         right--;
     }
+    return s;
 }
 
 int main(){
     string str;
     cin>>str;
-    isReverse(str);
+    cout<<isReverse(str);
     return 0;
 }
