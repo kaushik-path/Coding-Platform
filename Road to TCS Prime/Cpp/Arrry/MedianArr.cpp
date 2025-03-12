a6b8990de2ce12575bb18969ca039656777ae9ef
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-13 01:44:22 +0530

    push Median of array

--- Road to TCS Prime/Cpp/Arrry/MedianArr.cpp
+++ Road to TCS Prime/Cpp/Arrry/MedianArr.cpp
@@ -1,3 +1,12 @@
+/*Find Median of the given Array
+    Example 1:
+Input: [2,4,1,3,5]
+Output: 3
+
+    Example 2:
+Input: [2,5,1,7]
+Output: 3.5
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
