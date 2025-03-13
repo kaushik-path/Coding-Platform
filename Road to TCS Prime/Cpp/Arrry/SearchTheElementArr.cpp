8529305bf0a768794c4b78a6e23e0f136b6f3468
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-14 00:55:10 +0530

    +

--- Road to TCS Prime/Cpp/Arrry/SearchTheElementArr.cpp
+++ Road to TCS Prime/Cpp/Arrry/SearchTheElementArr.cpp
@@ -1,3 +1,12 @@
+/*Search an Element in an Array 
+    Example 1:
+Input: array[] = {1,2,3,4,5} k=3                                                                              Output: 2                                                                                                              Explanation: The answer is 2 because 3 is present at 2nd index.
+
+    Example 2:
+Input: array[]={6,7,9,5,3,10} k=10
+Output: 5
+Explanation: The answer is 5 because 10 is present at 5th index.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
