0f0f352ce8f32e42b3b4130ceb9ef57fbbace375
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-14 23:04:54 +0530

    New pyramid parttern

--- Road to TCS Prime/Cpp/Pattern/02_Pattern.cpp
+++ Road to TCS Prime/Cpp/Pattern/02_Pattern.cpp
@@ -40,7 +40,7 @@ int main(){
     cout<<endl;
 
     /*     
-     *
+     * 
     * *
    * * *
   * * * *
@@ -53,6 +53,32 @@ int main(){
         }
         for(int j=1; j<=i; j++){
             cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+//      *
+//     ***
+//    *****
+//   *******
+//    *****
+//     ***
+//      *
+    for(int i= 1; i<=n; i++){
+        for (int j = 1; j <=n-i; j++){
+            cout<<" ";
+        }
+        for (int j = 1; j <=2*i-1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i= n-1; i>=1; i--){
+        for (int j = 1; j <=n-i; j++){
+            cout<<" ";
+        }
+        for (int j = 1; j <=2*i-1; j++){
+            cout<<"*";
         }
         cout<<endl;
     }
