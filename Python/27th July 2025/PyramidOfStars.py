670981ea08dc2f2201191b4e453234b64028e922
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-07-27 01:44:06 +0530

    Pyramid of Stars (learn how to order the sequences)

--- Python/27th July 2025/PyramidOfStars.py
+++ Python/27th July 2025/PyramidOfStars.py
@@ -8,14 +8,9 @@
 # Expected Output:
 
 def Star(n):
-    for i in range(n-1,0,-1):
-        for j in range(i):
-            print(" ", end="")
-        print()
-        
-        for j in range(i):
-            print("*", end="")
-        print()
+    for i in range(1,n+1):
+        print((" " * (n-i))+ ("*" * (2*i-1)))
+
 
 n= int(input())
 Star(n)
