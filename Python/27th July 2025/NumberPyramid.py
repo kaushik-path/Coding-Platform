893a1469a339197d3a6dc0c45bd8ee8c0cb920e0
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-07-27 02:29:18 +0530

    will try this tomorrow

--- Python/27th July 2025/NumberPyramid.py
+++ Python/27th July 2025/NumberPyramid.py
@@ -1,2 +1,13 @@
-Q1. Number Pyramid
-For n = 4:
+# Q1. Number Pyramid
+# For n = 4:
+#    1
+#   121
+#  12321
+# 1234321
+
+def pyramid(n):
+    for i in range(1,n+1):
+        print(" " * (n-i) + (i * (n-i)))
+
+n= int(input())
+pyramid(n)
