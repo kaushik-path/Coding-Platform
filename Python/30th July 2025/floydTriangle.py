51c0b1281388824081b8400ab5c8af2ccd3d5808
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-07-30 01:20:34 +0530

    Floyd (learning how to print the count in a shape)

---
+++ Python/30th July 2025/floydTriangle.py
@@ -0,0 +1,18 @@
+# Floyd's Triangle
+# Print this pattern for input n = 5:
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+
+def Floyd(n):
+    count=1
+    for i in range(1, n+1):
+        for j in range(1,i+1):
+            print(count, end=" ")
+            count+=1
+        print()
+
+n= int(input())
+Floyd(n)
