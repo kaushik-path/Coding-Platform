82b4d5ce16eec39b86bb6b5bae5b7907d5105370
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-08-02 01:44:13 +0530

    revised

--- Python/PrintNnumdivby3.py
+++ Python/PrintNnumdivby3.py
@@ -1,7 +1,9 @@
 #Question 1: Print Numbers Divisible by 3 from 1 to 50
 #
 # Task: Write a Python program using a for loop to print all numbers between 1 and 50 that are divisible by 3.
+# 3 6 9 12 15 18 21 24 27 30 33 36 39 42 45 48 
 
 for i in range(1,51):
     if (i % 3==0):
-        print(i, end=" ") #<end=" " is used for print everyoutput in a single line>
+        print(i, end=" ") 
+        #<end=" " is used for print every output in a single line>
--- Python/floydTriangle.py
+++ Python/floydTriangle.py
@@ -9,7 +9,7 @@
 def Floyd(n):
     count=1
     for i in range(1, n+1):
-        for j in range(1,i+1):
+        for j in range(i):
             print(count, end=" ")
             count+=1
         print()
