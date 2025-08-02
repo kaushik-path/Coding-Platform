4313a7bf66c09308a2f4a4e6e8c065640bbc9747
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-08-03 02:20:48 +0530

    will try tomorrow

---
+++ Python/ElectricityBillCalculator.py
@@ -0,0 +1,15 @@
+# Question 3: Electricity Bill Calculator
+# Take input for:
+
+# Number of units consumed
+
+# Use this logic:
+
+# Units Consumed	Rate per unit
+# 0–100	                 ₹5
+# 101–200	             ₹7
+# 201–300	             ₹10
+# Above 300	             ₹15
+
+# Print total bill.
+
