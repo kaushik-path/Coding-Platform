5e4333cfa872363fab0c8094b9886912622d44db
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-08-04 02:08:03 +0530

    Electricity bil lCalculator (learning multiple task in a single line)

--- Python/ElectricityBillCalculator.py
+++ Python/ElectricityBillCalculator.py
@@ -13,3 +13,21 @@
 
 # Print total bill.
 
+def ElectricBill(n):
+    Total=0
+    if n>0:
+        if 1<= n <= 100:
+                Total = n * 5
+        elif 101<= n <=200:
+                Total = 100*5 + (n-100)*7
+        elif 201<= n <=300:
+                Total = 100*5 + 100*7 + (n-200)*10
+        else:
+                Total = 100*5 + 100*7 + 100*10 + (n-300)*15
+        print(Total)
+    else:
+        print("AAP")
+
+B=int(input())
+ElectricBill(B)
+
