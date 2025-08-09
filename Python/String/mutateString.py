32126ab188db79db3960456eebc0ca09602c6c4d
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-08-10 01:27:14 +0530

    Mutate String (change the value at the index of the stirng with the char)

---
+++ Python/String/mutateString.py
@@ -0,0 +1,34 @@
+# Read a given string, change the character at a given index and then print the modified string.
+# Function Description
+# Complete the mutate_string function in the editor below.
+# mutate_string has the following parameters:
+
+# string string: the string to change
+# int position: the index to insert the character at
+# string character: the character to insert
+# Returns
+
+# string: the altered string
+# Input Format
+
+# The first line contains a string, .
+# The next line contains an integer , the index location and a string , separated by a space.
+
+# Sample Input
+# STDIN           Function
+# -----           --------
+# abracadabra     s = 'abracadabra'
+# 5 k             position = 5, character = 'k'
+# Sample Output
+#     abrackdabra
+
+def MutateSting(Str, N, Char):
+    l = list(Str)
+    l[N] = Char
+    Str = ''.join(l)
+    return Str
+
+Str = str(input())
+N, Ch = input().split()
+res= MutateSting(Str, int(N), Ch)
+print(res)
