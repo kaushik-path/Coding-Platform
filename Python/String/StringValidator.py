b5be0cd9073bbd18b3ff83f83b1ba6d394690bbf
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-08-17 02:32:49 +0530

    String Validator (learning multiple in built function of str validation like all num, all digit, all lower, upper and alpha, learning about any() in a loop how to use and where to use)

---
+++ Python/String/StringValidator.py
@@ -0,0 +1,30 @@
+# Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+# str.isalnum() -> This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+# str.isalpha() -> This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+# str.isdigit() -> This method checks if all the characters of a string are digits (0-9).
+# str.islower() -> This method checks if all the characters of a string are lowercase characters (a-z).
+# str.isupper() -> This method checks if all the characters of a string are uppercase characters (A-Z).
+# Output Format
+    # In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+    # In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+    # In the third line, print True if  has any digits. Otherwise, print False.
+    # In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+    # In the fifth line, print True if  has any uppercase characters. Otherwise, print False
+# Sample Input
+    # qA2
+# Sample Output
+    # True
+    # True
+    # True
+    # True
+    # True
+
+def StringValidator(S):
+    print(any(c.isalnum() for c in S))
+    print(any(c.isdigit() for c in S))
+    print(any(c.islower() for c in S))
+    print(any(c.isupper() for c in S))
+    print(any(c.isalpha() for c in S))
+
+s = str(input())
+StringValidator(s)
